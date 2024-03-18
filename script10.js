da77f654ac8c344908116dfff3e82f33e6bea3b6
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-18 20:06:15 +0530

    updated

--- script10.js
+++ script10.js
@@ -1,1 +1,5 @@
 console.log("10) Change the text of a paragraph on a webpage using JavaScript.");
+
+const paragraph = document.getElementById("myParagraph");
+console.log(paragraph);
+paragraph.textContent = "This text has been changed using Javascript !";
--- script9.js
+++ script9.js
@@ -1,1 +1,13 @@
 console.log("9) Create an object representing a car with properties like make, model, and year.Add a method to the car object to start the engine.");
+
+let car = {
+    make:"Lamborghini",
+    model:"Countach",
+    year:2022
+}
+
+car.startEngine = function(){
+    console.log("Engine Started")
+}
+
+car.startEngine();
