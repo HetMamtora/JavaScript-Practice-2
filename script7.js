fab9a1db87d9f91471ea54b32b064cdc6adc5f18
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-18 20:00:18 +0530

    updated

--- script7.js
+++ script7.js
@@ -1,1 +1,14 @@
 console.log("7) Create a program that checks if a given year is a leap year.");
+
+const isLeapYear = (year) => {
+    if((year % 4 === 0  && year % 100 !==0) || year % 400 === 0){
+        return true;
+    } 
+    else{
+        return false
+    }
+}
+
+
+console.log("Year is 2024");
+console.log(isLeapYear(2024))
--- script8.js
+++ script8.js
@@ -1,1 +1,10 @@
 console.log("8) Find the sum of all elements in an array.");
+
+const arr = [20,30,40,10,8];
+
+let sum = 0 ;
+for(let i=0;i< arr.length;i++){
+    sum+=arr[i];
+}
+
+console.log(sum);   
