4d9cf8c94cf656e785108053b6a35ba439fd33a5
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-19 22:17:49 +0530

    updated

--- script12.js
+++ script12.js
@@ -1,1 +1,6 @@
 console.log("12) Add a click event listener to a button that displays an alert when clicked.");
+
+const button= document.querySelector("#buttonClick");
+button.addEventListener("click",()=>{
+    alert("Button Clicked")
+})
