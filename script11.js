ed137b75d1671d3c7c8e7897a09beb5f238fdda2
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-19 22:16:15 +0530

    Update script11.js

--- script11.js
+++ script11.js
@@ -1,6 +1,6 @@
 console.log("11) Create a button dynamically and add it to the webpage.");
 
-let container = document.getElementById("container");
-let button = document.createElement("button");
+const container = document.getElementById("container");
+const button = document.createElement("button");
 button.textContent = "Click Me";
 container.appendChild(button);
