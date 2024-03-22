b44fe9de116cf55069158e9733c92acaf6ed12c2
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-03-22 17:03:03 +0530

    updated

--- script37.js
+++ script37.js
@@ -1,2 +1,10 @@
 console.log("37) Write a regular expression to validate an email address.");
 
+const emailRegex = /^[a-zA-Z._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function validate(email){
+    return emailRegex.test(email);
+}
+
+const email = "abc@gmail.com";
+console.log(validate(email));
